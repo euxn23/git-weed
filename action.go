3fadb950cd7c507b220602c1752b2fb8ef4f171f
yutaszk <yutaszk23@gmail.com>
2017-09-14 20:10:02 +0900

    Add commit action

--- action.go
+++ action.go
@@ -2,9 +2,28 @@ package main
 
 import (
 	"github.com/urfave/cli"
+	"gopkg.in/src-d/go-git.v4"
+	"gopkg.in/src-d/go-git.v4/plumbing/object"
 )
 
 
 func Action(_ *cli.Context) error {
+	r, err := git.PlainOpen(".")
+	if err != nil {
+		return err
+	}
+
+	w, err := r.Worktree()
+	if err != nil {
+		return err
+	}
+
+	w.Commit(commitMessage, &git.CommitOptions{
+		Committer: &object.Signature{
+		},
+		Author: &object.Signature{
+		},
+	})
+
 	return nil
 }
