8a161238b28ae2bd341091ee1ffe9aaa604de952
yutaszk <yutaszk23@gmail.com>
2017-09-17 17:47:56 +0900

    Add timestamp edit

--- action.go
+++ action.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"time"
+
 	"github.com/motemen/go-gitconfig"
 	"github.com/urfave/cli"
 	"gopkg.in/src-d/go-git.v4"
@@ -19,6 +21,8 @@ func Action(_ *cli.Context) error {
 		return err
 	}
 
+	committedAt := editTimestamp()
+
 	userName, err := gitconfig.Default.GetString("user.name")
 	if err != nil {
 		return err
@@ -32,12 +36,48 @@ func Action(_ *cli.Context) error {
 		Committer: &object.Signature{
 			Name:  userName,
 			Email: userEmail,
+			When:  committedAt,
 		},
 		Author: &object.Signature{
 			Name:  userName,
 			Email: userEmail,
+			When:  committedAt,
 		},
 	})
 
 	return nil
+}
+
+func editTimestamp() time.Time {
+	committedAt := time.Now()
+	if random {
+		// TODO
+	}
+
+	if year == 0 {
+		year = committedAt.Year()
+	}
+	if month == 0 {
+		month = int(committedAt.Month())
+	}
+	if day == 0 {
+		day = committedAt.Day()
+	}
+	if hour == 0 {
+		hour = committedAt.Hour()
+	}
+	if minute == 0 {
+		minute = committedAt.Minute()
+	}
+	if second == 0 {
+		second = committedAt.Second()
+	}
+	committedAt = time.Date(year, time.Month(month), day, hour, minute, second, 0, committedAt.Location())
+
+	committedAt = committedAt.AddDate(-yearsAgo, -monthsAgo, -daysAgo)
+	committedAt = committedAt.Add(-time.Hour * time.Duration(hoursAgo))
+	committedAt = committedAt.Add(-time.Minute * time.Duration(minutesAgo))
+	committedAt = committedAt.Add(-time.Second * time.Duration(secondsAgo))
+
+	return committedAt
 }
